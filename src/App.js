fca66261d3e31663639aaaccdc76157e92d08b64
Hannes Kr. Hannesson <hanneskr4@gmail.com>
2018-02-13 16:41:42 +0000

    added text box and reindented file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    <ChatBox/>
+                </p>
+            </div>
+        );
+    }
+}
+
+class ChatBox extends Component {
+    constructor(props) {
+        super(props);
+        this.placeholder = "Enter text here...";
+    }
+    render() {
+        return <input placeholder={this.props.placeholder}></input>;
+    }
 }
 
 export default App;
